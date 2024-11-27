8c00d3a96e0e36b3493f14c350b3c39ef8b1c6ee
Alby <albyabraham926@gmail.com>
2024-11-28 02:22:11 +0530

    Lombok CHnages

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,9 +1,15 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //@Entity
 @Entity(name = "categories") // name field will help to map with the class with the mentioned Table name , otherwise simple class name will be the Tbale name by default
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -13,27 +19,4 @@ public class Category {
 //    @Column(name="Name")
     private String categoryName;
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
 }

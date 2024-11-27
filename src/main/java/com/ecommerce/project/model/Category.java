307228766f7af52b988e6a9901be2e26cccd73ba
Alby <albyabraham926@gmail.com>
2024-11-28 01:51:43 +0530

    Added H2 database , JPA dependency , Mapped the Category withe JPA Entity and repository , changed in service to interact with repository

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,8 +1,16 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+
+//@Entity
+@Entity(name = "categories") // name field will help to map with the class with the mentioned Table name , otherwise simple class name will be the Tbale name by default
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // this will automatic generate unique id value for this column when new record inserted, no need to maintain manual
     private Long categoryId;
+
+//    @Column(name="Name")
     private String categoryName;
 
     public Long getCategoryId() {
@@ -24,5 +32,8 @@ public class Category {
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+    }
+
+    public Category() {
     }
 }
--- src/main/java/com/ecommerce/project/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.service.impl;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repository.CategoryRepository;
 import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,42 +15,49 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> categories = new ArrayList<Category>();
-    private Long id=0L;
+//    private List<Category> categories = new ArrayList<Category>();
+//    private Long id=0L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        id++;
-        category.setCategoryId(id);
-        categories.add(category);
+//        id++;
+//        category.setCategoryId(id);
+//        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter( x -> x.getCategoryId().equals(categoryId)).findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found for Id : "+categoryId));
-
-        categories.remove(category);
+//        Category category = categories.stream()
+//                .filter( x -> x.getCategoryId().equals(categoryId)).findFirst()
+//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found for Id : "+categoryId));
+//
+//        categories.remove(category);
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found for Id : "+categoryId));
+        categoryRepository.delete(category);
         return "Category Deleted Successfully for CategoryId :"+categoryId;
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
 
-        Optional<Category> optionalCategory = categories.stream().filter(x -> x.getCategoryId().equals(categoryId)).findFirst();
+//        Optional<Category> optionalCategory = categories.stream().filter(x -> x.getCategoryId().equals(categoryId)).findFirst();
+//        Optional<Category> optionalCategory = categoryRepository.findById(categoryId);
+        Category savedCategory = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found for Id : "+categoryId));
 
-        if(optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        }else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found for Id : "+categoryId);
-        }
+//        categoryToUpdate.setCategoryName(category.getCategoryName());
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+
+
     }
 }

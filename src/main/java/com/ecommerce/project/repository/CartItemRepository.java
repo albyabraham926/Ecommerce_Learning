94f906e8e937372b7681b10fd56e9d0a24a516ef
Alby <albyabraham926@gmail.com>
2025-01-14 21:32:57 +0530

    5_Jan : P3 Update/Delete product from the user cart

--- src/main/java/com/ecommerce/project/repository/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repository/CartItemRepository.java
@@ -2,10 +2,16 @@ package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
     @Query("select ci from CartItem ci where ci.cart.cartId = ?1 and ci.product.productId = ?2")
     CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
+
+
+    @Modifying // we need to use this in case of deletion as we need to tell the jpa, we are intending to modify with below query, for select not required, but modify case require.
+    @Query("delete from CartItem ci where ci.cart.cartId = ?1 and ci.product.productId = ?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
